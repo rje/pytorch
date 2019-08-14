24961ee3db5a8c62e78e2da967255419d92823a9
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-08-13 17:36:57 -0700

    AddReLU module
    ghstack-source-id: 861b5f435eb1cb7ee2bf6161515ec3b408815af6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24217

--- torch/nn/_intrinsic/__init__.py
+++ torch/nn/_intrinsic/__init__.py
@@ -1,11 +1,17 @@
 # @lint-ignore-every PYTHON3COMPATIMPORTS
 
+from torch.nn._intrinsic.modules.fused import AddReLU
 from torch.nn._intrinsic.modules.fused import ConvBn2d
 from torch.nn._intrinsic.modules.fused import ConvBnReLU2d
 from torch.nn._intrinsic.modules.fused import ConvReLU2d
 from torch.nn._intrinsic.modules.fused import LinearReLU
 
+from . import quantized
+
 __all__ = [
+    'quantized',
+    #
+    'AddReLU',
     'ConvBn2d',
     'ConvBnReLU2d',
     'ConvReLU2d',
--- torch/nn/_intrinsic/modules/fused.py
+++ torch/nn/_intrinsic/modules/fused.py
@@ -1,4 +1,8 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import torch
 from torch.nn import Conv2d, ReLU, Linear, BatchNorm2d
 
@@ -25,3 +29,12 @@ class ConvBnReLU2d(torch.nn.Sequential):
         assert type(conv) == Conv2d and type(bn) == BatchNorm2d and \
             type(relu) == ReLU, 'Incorrect types for input modules{}{}{}' \
             .format(type(conv), type(bn), type(relu))
+
+
+class AddReLU(torch.nn.Module):
+    def __init__(self):
+        super(AddReLU, self).__init__()
+
+    def forward(self, a, b):
+        c = torch.add(a, b)
+        return torch.nn.functional.relu(c)
